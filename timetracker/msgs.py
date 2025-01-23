a5da38555e121c22b8349aa87215f2436e259f12
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-01-23 14:26:35 -0500

    Help for forcing an over-write is suppressed, as this a test-only feature

--- timetracker/msgs.py
+++ timetracker/msgs.py
@@ -7,6 +7,7 @@ __author__ = "DV Klopfenstein, PhD"
 def prt_started():
     """Message to print when the timer is started"""
     print('    Do `trkr stop -m "task description"` to stop tracking this time unit')
-    print('    Do `trkr start --force`              to reset start time to now')
+    ##Test feature
+    ##print('    Do `trkr start --force`              to reset start time to now')
 
 # Copyright (C) 2025-present, DV Klopfenstein, PhD. All rights reserved.

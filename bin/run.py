2a49933eda5f6fcb8dda7cb4a9ed8cf30a2c5607
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-01-25 15:45:21 -0500

    Update run for shorter trk

--- bin/run.py
+++ bin/run.py
@@ -4,7 +4,7 @@
 __copyright__ = 'Copyright (C) 2025, DV Klopfenstein, PhD. All rights reserved.'
 __author__ = "DV Klopfenstein, PhD"
 
-from timetracker.trkr import main
+from timetracker.trk import main
 
 
 if __name__ == '__main__':

be52cc28d7dd6503b26e594e12b7dfb65df6f4e7
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-01-28 12:50:43 -0500

    pylint

--- tests/test_cfg_global.py
+++ tests/test_cfg_global.py
@@ -4,20 +4,19 @@
 #from sys import argv
 #from os import environ
 ###from os import getcwd
-from os import system
 from os import makedirs
-from subprocess import run
-from os.path import exists
+##from os.path import exists
 from os.path import join
 from os.path import dirname
 from os.path import expanduser
+from subprocess import run
 #from collections import namedtuple
 #from datetime import timedelta
 #from timeit import default_timer
 from logging import basicConfig
 from logging import DEBUG
 from logging import debug
-import tempfile
+from tempfile import TemporaryDirectory
 from timetracker.cfg.cfg_global import CfgGlobal
 #from timetracker.cli import Cli
 #from timetracker.filemgr import FileMgr
@@ -27,36 +26,39 @@ basicConfig(level=DEBUG)
 
 
 def test_cfgbase_home():
+    """Test instantiating a default CfgGlobal"""
     cfg = CfgGlobal()
     assert cfg.fname == join(expanduser('~'), '.timetrackerconfig')
     assert cfg.docini['projects'] == []
-    cfg.wr_cfg()
     #system(f'cat {cfg.fname}')
 
 def test_cfgbase_temp():
+    """Test cfg flow"""
     # 1) INITIALIZE "HOME" DIRECTORY
-    tmp_home = tempfile.TemporaryDirectory()
-    cfg = CfgGlobal(tmp_home.name)
-    assert cfg.fname == join(tmp_home.name, '.timetrackerconfig'), f'{cfg.fname}'
-    assert cfg.docini['projects'] == []
-    # 2) WRITE AN EMPTY GLOBAL CONFIGURATION FILE
-    cfg.wr_cfg()
-    #debug(run(f'find {tmp_home.name}'.split(), capture_output=True, text=True).stdout)
-    # 3) READ THE EMPTY GLOBAL CONFIGURATION FILE
-    doc = cfg.rd_cfg()
-    assert doc['projects'] == cfg.docini['projects']
-    debug(run(f'cat {cfg.fname}'.split(), capture_output=True, text=True).stdout)
-    # 4) Add local project config file
+    with TemporaryDirectory() as tmp_home:
+        cfg = CfgGlobal(tmp_home)
+        assert cfg.fname == join(tmp_home, '.timetrackerconfig'), f'{cfg.fname}'
+        assert cfg.docini['projects'] == []
+        # 2) WRITE AN EMPTY GLOBAL CONFIGURATION FILE
+        cfg.wr_cfg()
+        #debug(run(f'find {tmp_home}'.split(), capture_output=True, text=True, check=True).stdout)
+        # 3) READ THE EMPTY GLOBAL CONFIGURATION FILE
+        doc = cfg.rd_cfg()
+        assert doc['projects'] == cfg.docini['projects']
+        debug(run(f'cat {cfg.fname}'.split(), capture_output=True, text=True, check=True).stdout)
+        # 4) Create local project directories
+        _mk_dirs(tmp_home)
+
 
 def test_cfgdir():
     """Test the TimeTracker global configuration"""
     cfg = CfgGlobal()
     assert dirname(cfg.fname) == expanduser('~')
 
-    tmp_home = tempfile.TemporaryDirectory()
-    cfg = CfgGlobal(cfgdir=tmp_home.name)
-    assert dirname(cfg.fname) == tmp_home.name
-    tmp_home.cleanup()
+    with TemporaryDirectory() as tmp_home:
+        cfg = CfgGlobal(cfgdir=tmp_home.name)
+        assert dirname(cfg.fname) == tmp_home.name
+        tmp_home.cleanup()
 
 def _mk_dirs(tmp_home):
     projs = {
@@ -66,7 +68,7 @@ def _mk_dirs(tmp_home):
     }
     for pdir in projs.values():
         makedirs(pdir)
-    system(f'find {tmp_home.name}')
+    debug(run(f'find {tmp_home.name}'.split(), capture_output=True, text=True, check=True).stdout)
 
 if __name__ == '__main__':
     #test_cfgdir()

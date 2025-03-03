0cf88a63a08773bf4de638b63b60303c5df0fc29
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-03-03 02:34:43 -0500

    Working through report

--- timetracker/cmd/report.py
+++ timetracker/cmd/report.py
@@ -3,7 +3,6 @@
 __copyright__ = 'Copyright (C) 2025-present, DV Klopfenstein, PhD. All rights reserved.'
 __author__ = "DV Klopfenstein, PhD"
 
-from os.path import exists
 from logging import debug
 
 from timetracker.cmd.common import get_fcsv
@@ -15,32 +14,43 @@ from timetracker.timetext import get_data_formatted
 
 def cli_run_report(fnamecfg, args):
     """Report all time units"""
-    if exists(args.input):
-        run_io(args.input, args.output)
-        return
-    run_report(
-        fnamecfg,
-        args.name,
-        fin=args.input,
-        fout=args.output,
-    )
+    if args.input is None:
+        run_report(fnamecfg, args.name)
+    elif len(args.input) == 1:
+        run_io(args.input[0], args.output)
+    else:
+        raise RuntimeError('TIME TO IMPLEMENT')
+    ##if args.input and exists(args.input):
+    ##    print(args.input)
+    ##if args.input and args.output and exists(args.input):
+    ##    run_io(args.input, args.output)
+    ##    return
+    ##run_report(
+    ##    fnamecfg,
+    ##    args.name,
+    ##    fin=args.input,
+    ##    fout=args.output,
+    ##)
 
-def run_report(fnamecfg, uname, **kwargs):
+def run_report(fnamecfg, uname, dirhome=None):
     """Report all time units"""
     debug(yellow('RUNNING COMMAND REPORT'))
-    fcsv = get_fcsv(fnamecfg, uname, kwargs.get('dirhome'))
+    fcsv = get_fcsv(fnamecfg, uname, dirhome)
     return run_io(fcsv, None) if fcsv is not None else None
 
 def run_io(fcsv, fout_docx):
     """Run input output"""
     ocsv = CsvFile(fcsv)
     timedata = ocsv.get_data()
+    for e in sorted(timedata, key=lambda nt: nt.start_datetime):
+        print(e)
     timefmtd = get_data_formatted(timedata)
-    if timedata and fout_docx:
-        doc = WordDoc(timefmtd)
-        for e in doc.nttext:
+    if timefmtd:
+        for e in timefmtd:
             print(e)
-        doc.write_doc(fout_docx)
+        if fout_docx:
+            doc = WordDoc(timefmtd)
+            doc.write_doc(fout_docx)
 
 
 # Copyright (C) 2025-present, DV Klopfenstein, PhD. All rights reserved.

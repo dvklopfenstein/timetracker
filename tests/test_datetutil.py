ca36e55d21744971c56a4c3edc4ef092686efb28
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-03-08 08:48:43 -0500

    Added test for dateutil issue 1421 that I opened

---
+++ tests/test_datetutil.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Test dateutil interpreting 5pm"""
+# https://github.com/dateutil/dateutil/issues/1421
+
+from datetime import datetime
+from dateutil.parser import parse
+
+
+def test_dateutil():
+    """Test dateutil interpreting 5pm"""
+    defaultdt = datetime(2025, month=1, day=1, hour=8, minute=15)
+
+    dt5  = parse("01-1 5pm")
+    dt5a = parse("01-1 5pm", default=defaultdt)
+
+    print(dt5)
+    print(dt5a)
+    # pylint: disable=fixme
+    # TODO: If dateutil issue 1421 is fixed, uncomment
+    #assert dt5 == dt5a
+
+
+if __name__ == '__main__':
+    test_dateutil()

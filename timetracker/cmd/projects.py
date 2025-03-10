e7227b941faaa7271392dcb60ccbdddfb3431772
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-03-10 17:43:54 -0400

    Renamed command: csv -> projects

--- timetracker/cmd/projects.py
+++ timetracker/cmd/projects.py
@@ -6,37 +6,39 @@ __author__ = "DV Klopfenstein, PhD"
 from os.path import exists
 from logging import debug
 from timetracker.utils import yellow
+from timetracker.cfg.utils import get_dirhome_globalcfg
 from timetracker.cfg.cfg_local  import CfgProj
 from timetracker.cfg.cfg_global import CfgGlobal
 from timetracker.msgs import str_init
 
 
-def cli_run_csv(fnamecfg, args):
+def cli_run_projects(fnamecfg, args):
     """Stop the timer and record this time unit"""
-    run_csv(
+    run_projects(
         fnamecfg,
         args.name)
 
-def run_csv(fnamecfg, uname, dirhome=None):
+def run_projects(fnamecfg, uname, dirhome=None):
     """Stop the timer and record this time unit"""
     # Get the starting time, if the timer is running
-    debug(yellow('RUNNING COMMAND CSV'))
+    debug(yellow('RUNNING COMMAND PROJECTS'))
+    dirhome = get_dirhome_globalcfg()
     if not exists(fnamecfg):
         print(str_init(fnamecfg))
     cfgproj = CfgProj(fnamecfg, dirhome=dirhome)
 
     # List location of the timetracker file with this time unit
     if uname == 'all':
-        _get_csv_proj_all(cfgproj)
+        _get_proj_all(cfgproj)
     else:
-        _get_csv_proj_user(cfgproj, uname)
+        _get_proj_user(cfgproj, uname)
 
-def _get_csv_proj_user(cfgproj, uname):
+def _get_proj_user(cfgproj, uname):
     fcsv = cfgproj.get_filename_csv(uname)
     if fcsv is not None:
         print(f'CSV exists({int(exists(fcsv))}) {fcsv}')
 
-def _get_csv_proj_all(cfgproj):
+def _get_proj_all(cfgproj):
     fcsvs = cfgproj.get_project_csvs()
     for fcsv in fcsvs:
         if fcsv is not None:

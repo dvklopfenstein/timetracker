52f6fb022391665d57aab99cf162875547d75fdb
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-03-13 15:08:52 -0400

    user-friendly do init msg

--- timetracker/msgs.py
+++ timetracker/msgs.py
@@ -56,7 +56,7 @@ def str_init_empty_proj(cfglocal):
 def str_init(fnamecfg):
     """Message that occurs when there is no Timetracking config file"""
     return ('Run `trk init` to initialize time-tracking '
-           f'for the project in {dirname(fnamecfg)}')
+           f'for the project in {dirname(dirname(fnamecfg))}')
 
 def str_notrkrepo_mount(mountname, trkdir):
     """Message when researcher is not in a dir or subdir that is managed by trk"""

5c0af7e894efac741db26ce273fafac18721aee0
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-05-07 16:58:01 -0400

    Added fnc, set_cfg_global, to Cfg; run_projects uses set_cfg_global; added fnc, get_projects, to Cfg

--- timetracker/cfg/cfg.py
+++ timetracker/cfg/cfg.py
@@ -6,10 +6,11 @@ __author__ = "DV Klopfenstein, PhD"
 from os.path import exists
 from logging import debug
 from timetracker.cfg.cfg_global import get_cfgglobal
-#from timetracker.cfg.cfg_global import CfgGlobal
+from timetracker.cfg.cfg_global import CfgGlobal
 from timetracker.cfg.cfg_local  import CfgProj
 from timetracker.cfg.doc_local import get_docproj
 from timetracker.cfg.doc_local import get_ntdocproj
+from timetracker.cfg.docutils import get_value
 from timetracker.cfg.utils import get_filename_globalcfg
 from timetracker.utils import yellow
 #from timetracker.msgs import str_tostart
@@ -41,6 +42,17 @@ class Cfg:
     ##    #    print(str_reinit())
     ##    #return True
 
+    def get_projects(self, dirhome=None, fcfg_global=None):
+        """Get a list of projects from the global config file"""
+        if self.cfg_glb is None:
+            self.set_cfg_global(fcfg_global, dirhome)
+        return self.cfg_glb.get_projects()
+
+    def set_cfg_global(self, fcfg_global, dirhome=None):
+        """Create and set `cfg_glb` with a CfgGlobal object"""
+        fcfg_doc = get_value(get_docproj(self.cfg_loc.filename), 'global_config', 'filename')
+        self.cfg_glb = get_cfgglobal(fcfg_global, dirhome, fcfg_doc)
+
     def needs_reinit(self, dircsv, project, fcfg_global, dirhome=None):
         """Check to see if CfgProj needs to be re-initialized"""
         debug(yellow(f'Cfg.needs_reinit({dircsv=}, {project=}, {fcfg_global=}, {dirhome=})'))
@@ -55,7 +67,7 @@ class Cfg:
         # pylint: disable=line-too-long
         ##if fcfg_global is not None and (fcfgg_orig := docproj.global_config_filename) != fcfg_global:
         if fcfg_global is not None and \
-            (fcfgg_orig := get_filename_globalcfg(fcfg_doc=docproj.global_config_filename)) != fcfg_global:
+            (fcfgg_orig := get_filename_globalcfg(fcfg_doc=docproj.global_config_filename, msg="Cfg.needs_reinit")) != fcfg_global:
             msg.append(f'  * change the global config filename\n'
                        f'        from: "{fcfgg_orig}"\n'
                        f'        to:   "{fcfg_global}"')
@@ -79,7 +91,7 @@ class Cfg:
         self.cfg_loc.wr_ini_file(project, dircsv, fcfg_global)
         print(f'Initialized project directory: {self.cfg_loc.dircfg}')
         if self.cfg_glb is None:
-            self.cfg_glb = get_cfgglobal(fcfg_global, dirhome)
+            self.set_cfg_global(fcfg_global, dirhome)
         debug(f'INIT CfgGlobal filename {self.cfg_glb.filename}')
         return self.cfg_glb.wr_ini_project(project, self.cfg_loc.filename)
 
@@ -89,18 +101,17 @@ class Cfg:
                      f'fcfg_global={fcfg_global}, dirhome={dirhome})'))
         assert self.cfg_loc is not None
 
-        fcfg_glb_cur = self._get_fcfg_glb(dirhome, fcfg_global)
-        debug(yellow(f'Cfg._reinit {fcfg_glb_cur=}'))
+        # pylint: disable=line-too-long
+        ##debug(yellow(f"Cfg._reinit fcfg_glb_cur={self._get_fcfg_glb(dirhome, fcfg_global, 'DEBUG reinit.cur')}"))
         self._reinit_loc_main(project, dircsv, fcfg_global, dirhome)
 
-        fcfg_glb_new = self._get_fcfg_glb(dirhome, fcfg_global)
-        debug(yellow(f'Cfg._reinit {fcfg_glb_new=}'))
+        ##debug(yellow(f"Cfg._reinit fcfg_glb_new={self._get_fcfg_glb(dirhome, fcfg_global, 'DEBUG reinit.new')}"))
         self._reinit_glb_main(fcfg_global, dirhome, self.cfg_loc.filename)
 
-    def _get_fcfg_glb(self, dirhome, fcfg_global):
+    def _get_fcfg_glb(self, dirhome, fcfg_global, msg):
         docproj = get_docproj(self.cfg_loc.filename)
         fcfg_doc = docproj.global_config_filename if docproj else None
-        return get_filename_globalcfg(dirhome, fcfg_global, fcfg_doc)
+        return get_filename_globalcfg(dirhome, fcfg_global, fcfg_doc, msg)
 
     # pylint: disable=unknown-option-value,too-many-arguments,too-many-positional-arguments
     def _reinit_loc_main(self, project, dircsv, fcfg_global, dirhome):
@@ -119,7 +130,8 @@ class Cfg:
 
     def _reinit_glb_main(self, fcfg_global, dirhome, fcfg_loc):
         docproj = get_docproj(fcfg_loc)
-        fcfg_glb = get_filename_globalcfg(dirhome, fcfg_global, docproj.global_config_filename)
+        # pylint: disable=line-too-long
+        fcfg_glb = get_filename_globalcfg(dirhome, fcfg_global, docproj.global_config_filename, "Cfg._reinit_glb_main")
         assert fcfg_glb is not None
         if self.cfg_glb:
             debug(yellow(f'_reinit_global {self.cfg_glb.filename=}'))
@@ -130,17 +142,16 @@ class Cfg:
         ##if self.cfg_glb is None and fcfg_global is None:
         ##    cfg_glb.wr_ini_project(docproj.project, fcfg_loc)
         ##elif self.cfg_glb is not None and fcfg_global is None:
-        self._reinit_glb_exp0(docproj.project, fcfg_global, dirhome, fcfg_loc)
-
-    def _reinit_glb_exp0(self, project, fcfg_global, dirhome, fcfg_loc):
+        ####self._reinit_glb_exp0(docproj.project, fcfg_global, dirhome, fcfg_loc, docproj)
+        ####def _reinit_glb_exp0(self, project, fcfg_global, dirhome, fcfg_loc, docproj):
+        debug(yellow(f'_reinit_glb_exp0(\n  {docproj.project=},\n  {fcfg_global=},\n  {dirhome=},\n  {fcfg_loc=})'))
         if self.cfg_glb is None:
-            self.cfg_glb = get_cfgglobal(fcfg_global, dirhome)
-            ##self.cfg_glb = CfgGlobal(fcfg_glb)
+            self.cfg_glb = CfgGlobal(fcfg_glb)
 
         if not exists(self.cfg_glb.filename):
-            self.cfg_glb.wr_ini_project(project, fcfg_loc)
+            self.cfg_glb.wr_ini_project(docproj.project, fcfg_loc)
         else:
-            self.cfg_glb.reinit(project, fcfg_loc)
+            self.cfg_glb.reinit(docproj.project, fcfg_loc)
 
     @staticmethod
     def _needs_reinit_fcsv(docproj, dircsv):

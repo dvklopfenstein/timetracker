963f4dbf53e072124d04dba21a127c9b8dc7126d
DVKlopfenstein <dvklopfenstein@users.noreply.github.com>
2025-05-20 14:30:16 -0400

    Added checking for get_csvs_global_uname and get_csvs_local_all

--- tests/pkgtttest/expcsvs.py
+++ tests/pkgtttest/expcsvs.py
@@ -14,7 +14,6 @@ class ExpCsvs:
 
     def chk_get_csvs_global_all(self, key2ntcsvs):
         """Check the csvs resulting from `get_csvs_global_uname`"""
-        print(key2ntcsvs)
         for usr_act, ntcsvs_act in key2ntcsvs.items():
             ntcsvs_exp = self._exp_get_csvs_global_all(usr_act)
             fcsvs_exp = set(nt.fcsv for nt in ntcsvs_exp)
@@ -23,28 +22,48 @@ class ExpCsvs:
             for ntcsv in ntcsvs_act:
                 assert usr_act in ntcsv.fcsv, f'FILE OUTSIDE OF ACCOUNT({usr_act}): {ntcsv.fcsv}'
                 #print(f'GLB ALL ACT {usr_act:7} {ntcsv}')
-            print('')
 
     def chk_get_csvs_global_uname(self, key2ntcsvs):
         """Check the csvs resulting from `get_csvs_global_uname`"""
-        print(key2ntcsvs)
-        for (usr_act, prj_act), ntcsvs in key2ntcsvs.items():
-            self._exp_get_csvs_global_uname(usr_act)
-            for ntcsv in ntcsvs:
-                print(f'ACT {usr_act:7} {prj_act:11} {ntcsv}')
-            print('')
+        for usr_act, ntcsvs_act in key2ntcsvs.items():
+            ntcsvs_exp = self._exp_get_csvs_global_uname(usr_act)
+            assert ntcsvs_act == ntcsvs_exp, self._err(ntcsvs_act, ntcsvs_exp)
+            for ntcsv in ntcsvs_act:
+                assert ntcsv.username == usr_act
+                #print(f'ACT {usr_act:7} {ntcsv}')
 
+    def chk_get_csvs_local_all(self, usrprj2ntcsvs):
+        """Check the csvs resulting from `get_csvs_global_uname`"""
+        for (usr_act, prj_act), ntcsvs_act in usrprj2ntcsvs.items():
+            ntcsvs_exp = self._exp_get_csvs_local_all(usr_act, prj_act)
+            fcsvs_exp = set(nt.fcsv for nt in ntcsvs_exp)
+            fcsvs_act = set(nt.fcsv for nt in ntcsvs_act)
+            assert fcsvs_act == fcsvs_exp, self._err(fcsvs_act, fcsvs_exp)
+            for ntcsv in ntcsvs_act:
+                assert usr_act in ntcsv.fcsv, f'FILE OUTSIDE OF ACCOUNT({usr_act}): {ntcsv.fcsv}'
+                #print(f'GLB ALL ACT {usr_act:7} {ntcsv}')
+
+    # -------------------------------------------------------------------------------
     def _exp_get_csvs_global_uname(self, uname):
-        for ntcsv in self.exp_ntcsvs:
-            if ntcsv.username == uname:
-                print(f'EXP: {uname} {ntcsv}')
+        nts = set()
+        for ntcsv in self._exp_get_csvs_global_all(uname):
+            if f'_{uname}.csv' in ntcsv.fcsv:
+                nts.add(ntcsv)
+        return nts
 
     def _exp_get_csvs_global_all(self, login):
-        ret = []
+        nts = set()
         for ntcsv in self.exp_ntcsvs:
             if ntcsv.fcsv is not None and f'home/{login}/proj' in ntcsv.fcsv:
-                ret.append(ntcsv)
-        return ret
+                nts.add(ntcsv)
+        return nts
+
+    def _exp_get_csvs_local_all(self, login, project):
+        nts = set()
+        for ntcsv in self.exp_ntcsvs:
+            if ntcsv.fcsv is not None and f'home/{login}/proj/{project}' in ntcsv.fcsv:
+                nts.add(ntcsv)
+        return nts
 
     def _err(self, act, exp):
         errmsg = [f'ACT[{len(act)}] != EXP[{len(exp)}]',]
